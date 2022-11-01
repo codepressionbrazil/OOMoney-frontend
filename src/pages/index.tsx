b7928fd86cd6ce24383b56340786e4c9b39d6bb9
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-10-31 23:03:23 -0300

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>OOMoney</title>
+        <meta name="description" content="Transactions register app" />
+        <link rel="icon" href="/salary.png" />
+      </Head>
+
+      <main className="grid min-h-screen place-items-center text-center">
+        <div>
+          <h2>Wellcome to OOMoney</h2>
+          <p>A object oriented transactions register.</p>
+          <p>Our main goal is to make yout money control easier!</p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;

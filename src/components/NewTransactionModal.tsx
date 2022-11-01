93eb9b899c9a6eb72233d0a780f3e287bde53229
gabrielforster <rochafrgabriel@gmail.com>
2022-11-01 12:17:46 -0300

    feat: init newTransactionModal component

--- src/components/NewTransactionModal.tsx
+++ src/components/NewTransactionModal.tsx
@@ -12,9 +12,9 @@ import { KIND } from "baseui/button";
 
 import { FormControl } from "baseui/form-control";
 import { Input } from "baseui/input";
-import { PhoneInput, COUNTRIES, SIZE as PHONESIZE } from "baseui/phone-input";
-import { Select, Value } from "baseui/select";
-import { Checkbox, LABEL_PLACEMENT } from "baseui/checkbox";
+import { DatePicker } from "baseui/datepicker";
+
+import locale from "date-fns/locale/pt-BR";
 
 interface NewTransactionModalProps{
   isOpen: boolean;
@@ -24,11 +24,50 @@ interface NewTransactionModalProps{
 export function NewTransactionModal(props: NewTransactionModalProps){
   
   const [title, setTitle] = useState<string>("")
+  const [value, setValue] = useState<number>(0)
+  const [date, setDate] = useState<Date>(new Date())
 
   return (
     <Modal
       isOpen={props.isOpen}
+      onClose={props.onClose}
     >
+      <ModalHeader>Adicionar transação</ModalHeader>
+      <ModalBody>
+        <FormControl label="Título">
+          <Input
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}
+          />
+        </FormControl>
+
+        <FormControl label="Valor">
+          <Input
+            value={value}
+            onChange={(e) => setValue(Number(e.currentTarget.value))}
+          />
+        </FormControl>
+
+        <FormControl label="Data">
+          <DatePicker
+            value={date}
+            onChange={({ date }) => setDate(date as Date)}
+            formatString="dd/MM/yyyy"
+            placeholder="dd/MM/yyyy"
+            locale={locale}
+          />
+        </FormControl>
+      </ModalBody>
+
+      <ModalFooter>
+        <ModalButton kind={KIND.tertiary} onClick={props.onClose}>
+          Cancelar
+        </ModalButton>
+        
+        <ModalButton>
+          Adicionar
+        </ModalButton>
+      </ModalFooter>
 
     </Modal>
   )

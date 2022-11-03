fee9beef25285b12fe587bf533e1386d33789148
gabrielforster <rochafrgabriel@gmail.com>
2022-11-03 10:24:08 -0300

    feat: change auth hook file name

---
+++ src/context/useAuth.ts
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { api } from "../services/api";
+
+interface User {
+  idConta: number;
+  nome: string;
+  usuario: string;
+  email: string;
+  senha?: string;
+}
+
+interface registerProps {
+  nome: string,
+  usuario: string,
+  email: string,
+  senha: string,
+  telefone: number,
+  cpf: number
+}
+
+export function useAuth() {
+  const router = useRouter()
+
+  const [user, setUser] = useState<User | null>(getUserFromSessionStorage());
+
+  async function execRegister({
+    nome,
+    usuario,
+    email,
+    senha,
+    telefone,
+    cpf
+  }: registerProps) {
+    try {
+      const response = await api.post('/conta', {
+        nome, email, senha, telefone, cpf, usuario
+      })
+
+      if(response.status === 200) {
+        router.push('/login')
+      }
+
+      if(response.status >= 400) {
+        throw new Error(response.data.message)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function execLogin(email: string, password: string): Promise<void> {
+    try {
+      const { data } = await api.post("/login", { email, password });
+      saveUserToSessionStorage(data);
+      setUser(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function execLogout(): Promise<void> {
+    setUser(null);
+  }
+
+  function getUserFromSessionStorage() {
+    const user = sessionStorage.getItem("user-oo-money");
+    return user ? JSON.parse(user) : null
+  }
+
+  function saveUserToSessionStorage(userData: User): void {
+    sessionStorage.setItem("user-oo-money", JSON.stringify(userData));
+  }
+
+  return { user, execRegister, execLogin, execLogout, getUserFromSessionStorage };
+}

59c270f97e87ebf3c484d67e5bfd01818dd50f31
gabrielforster <rochafrgabriel@gmail.com>
2022-11-03 09:14:59 -0300

    feat: cadastro page

--- src/context/useTransaction.tsx
+++ src/context/useTransaction.tsx
@@ -1,4 +1,4 @@
-import React,{ createContext, ReactElement, ReactNode, useContext, useEffect, useState } from 'react'
+import React,{ createContext, useContext, useEffect, useState } from 'react'
 
 import * as types from "../types/types"
 import { api } from "../services/api"
--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -1,22 +1,106 @@
 import type { NextPage } from "next";
+import { useState } from "react"
+import { useRouter } from "next/router";
 
-const Cadastro:NextPage = () => {
+import { FormControl } from "baseui/form-control";
+import { Input, SIZE as InputSize } from "baseui/input";
+import { Button, KIND as ButtonKind } from "baseui/button";
 
-	return (
-		<>
-            <img src="" alt="" className=""/>
-            <form action="">
-            <input type="text" placeholder="Nome completo" />
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="CPF" />
-            <input type="number" placeholder="Telefone" />
-            <input type="text" placeholder="Nome de usuário" />
-            <input type="password" placeholder="Senha" />
-            <input type="password" placeholder="Confirme a senha" />
-            </form>
-            <button className="">Cadastre-se</button>
-		</>
-	)
+import { api } from "../services/api";
+
+export default function Cadastro(){
+
+  const [nome, setNome] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [senha, setSenha] = useState<string>('')
+  const [confirmarSenha, setConfirmarSenha] = useState<string>('')
+  const [telefone, setTelefone] = useState<string>('')
+  const [cpf, setCpf] = useState<string>('')
+  const [usuario, setUsuario] = useState<string>('')
+
+  const router = useRouter();
+
+  async function cadastrarUsuario() {
+    const response = await api.post('/usuarios', {
+      nome,
+      email,
+      senha,
+      confirmarSenha,
+      telefone,
+      cpf,
+      usuario
+    })
+
+    router.replace('/login')
+  }
+
+  return(
+    <main className="mt-1 flex flex-col justify-center items-center">
+      <h1 className="text-1xl">Cadastre-se!</h1>
+      <form method="POST" className="w-[30%]">
+        <FormControl label="Nome">
+          <Input
+            value={nome}
+            onChange={(e) => setNome(e.currentTarget.value)}
+            placeholder="Nome"
+            size={InputSize.compact}
+          />
+        </FormControl>
+        <FormControl label="Email">
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+            placeholder="Email"
+            size={InputSize.compact}
+          />
+        </FormControl>
+        <FormControl label="Senha">
+          <Input
+            value={senha}
+            onChange={(e) => setSenha(e.currentTarget.value)}
+            placeholder="Senha"
+            size={InputSize.compact}
+          />
+        </FormControl>
+        <FormControl label="Confirmar Senha">
+          <Input
+            value={confirmarSenha}
+            onChange={(e) => setConfirmarSenha(e.currentTarget.value)}
+            placeholder="Confirmar Senha"
+            size={InputSize.compact}
+          />
+        </FormControl>
+        <FormControl label="Telefone">
+          <Input
+            value={telefone}
+            onChange={(e) => setTelefone(e.currentTarget.value)}
+            placeholder="Telefone"
+            size={InputSize.compact}
+          />
+        </FormControl>
+        
+        <FormControl label="CPF">
+          <Input
+            value={cpf}
+            onChange={(e) => setCpf(e.currentTarget.value)}
+            placeholder="CPF"
+            size={InputSize.compact}
+          />
+        </FormControl>
+
+        <FormControl label="Usuário">
+          <Input
+            value={usuario}
+            onChange={(e) => setUsuario(e.currentTarget.value)}
+            placeholder="Usuário"
+            size={InputSize.compact}
+          />
+        </FormControl>
+        <div className="flex justify-between">
+          <Button type="reset" kind={ButtonKind.secondary}>Cancelar</Button>
+          <Button type="submit" onClick={cadastrarUsuario}>Cadastrar</Button>
+        </div>
+      </form>
+    </main>
+  )
 }
-
-export default Cadastro;

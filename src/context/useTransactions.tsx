2f83142e9875046b8684f7fc1c2b5e388ba999e0
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-11-15 19:04:10 -0300

    feat: init edit and delete transaction logic

--- src/context/useTransactions.tsx
+++ src/context/useTransactions.tsx
@@ -8,6 +8,8 @@ interface TransactionContextData {
   transactions: types.TransactionFromDB[]
   transactionClassifications: types.TransactionClassification[]
   createTransaction: (transaction: TransactionInput) => Promise<void>
+  updateTransaction: (transaction: types.updateData, transactionId: number) => Promise<void>
+  deleteTransaction: (transactionId: number) => Promise<void>
   getTransactionClassifications: () => Promise<void>
 }
 
@@ -67,8 +69,42 @@ export function TransactionProvider({children}: any): JSX.Element {
     }   
   }
 
+  async function updateTransaction(data: types.updateData, transactionId: number){
+    try {
+
+      const formattedData = {
+        descricao: data.description,
+        valorTransacao: data.amount,
+        dataHora: data.date,
+        tipoTransacao: data.transactionType,
+        classificacao: {
+          idClassificacao: data.idClassification
+        },
+        pessoa: {
+          cpf: user?.cpf
+        }
+      }
+
+      await api.put(`/transacao/${transactionId}`, formattedData)
+
+      await getTransactions()
+      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function deleteTransaction(transactionId: number){
+    try {
+      await api.delete(`/transacao/${transactionId}`)
+      await getTransactions()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <TransactionContext.Provider value={{ transactions, transactionClassifications, getTransactionClassifications, createTransaction }}>
+    <TransactionContext.Provider value={{ transactions, transactionClassifications, getTransactionClassifications, createTransaction, updateTransaction, deleteTransaction }}>
       {children}
     </TransactionContext.Provider>
   )
